e9846e2202c2e8377abb3af3ed63c056f4a3fc32
Aryaveer0305 <73471805+Aryaveer0305@users.noreply.github.com>
2021-01-22 17:20:43 +0530

    Add files via upload

---
+++ TowerSiege-1-main/sketch.js
@@ -0,0 +1,13 @@
+var stand
+function setup()
+{
+    ground = new Ground(600,height,1200,20);var
+    stand = (200,250,100,10)
+}
+function draw()
+{
+    display()
+{
+    stand.display();
+}    
+}
